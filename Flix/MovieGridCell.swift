fe2a8bc2e53a07b373d270d4c791118296a92a39
chen kong <kongchen2012@gmail.com>
2020-10-02 21:47:03 -0700

    assignment2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Chen Kong on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
